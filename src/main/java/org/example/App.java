5b55f9c6d4b6db3814b90f9962a879e9d60c7d61
andre <andrew.a.rubin@gmail.com>
2020-12-19 18:09:03 +0200

    init folder search

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,13 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println(args[0] + " " + args[1]);
-     /*   GtAwsDirectory awsDirectory = new GtAwsDirectory("18.203.171.233", "home");
-        awsDirectory.executeCommand();
-*/
+
         if( args.length >= 2) {
+            System.out.println(args[0] + " " + args[1]);
             GtAwsDirectory awsDirectory = new GtAwsDirectory(args[0], args[1]);
             awsDirectory.executeCommand();
         }else{
